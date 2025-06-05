9b7e382c24b6746405f0c90e328062d9f3c57073
Eng-Hasan46 <itzkepro@gmail.com>
2025-06-06 02:38:40 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function openPage(project) {
+  if (project === 'birthday') {
+    window.open('path/to/birthday/index.html', '_blank');
+  } else if (project === 'movies') {
+    window.open('path/to/movies/index.html', '_blank');
+  }
+}
